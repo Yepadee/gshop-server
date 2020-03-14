3ff94687db3612ed223bbc4912b6288890cd14ec
James <james@hawkinsplace.co.uk>
2020-03-14 22:26:15 +0000

    added mutations

--- schema.js
+++ schema.js
@@ -10,7 +10,7 @@ const {
     GraphQLNonNull
 } = require('graphql')
 
-const Product = new GraphQLObjectType({
+const ProductType = new GraphQLObjectType({
     name: 'Product',
     description: 'A product for sale in the shop',
     fields: () => ({
@@ -37,11 +37,23 @@ const Product = new GraphQLObjectType({
             resolve: (product) => {
                 return product.price;
             }
+        },
+        discount: {
+            type: GraphQLFloat,
+            resolve: (product) => {
+                return product.discount;
+            }
+        },
+        stock: {
+            type: new GraphQLList(StockType),
+            resolve: (product) => {
+                return product.getStocks();
+            }
         }
     })
 });
 
-const Stock = new GraphQLObjectType({
+const StockType = new GraphQLObjectType({
     name: 'Stock',
     description: 'The stock of a variant of product',
     fields: () => ({
@@ -57,10 +69,22 @@ const Stock = new GraphQLObjectType({
                 return stock.quantity;
             }
         },
+        stockProperties: {
+            type: new GraphQLList(StockPropertyType),
+            resolve: (stock) => {
+                return stock.getStockProperties();
+            }
+        },
+        product: {
+            type: ProductType,
+            resolve: (stock) => {
+                return stock.getProduct();
+            }
+        }
     })
 });
 
-const StockProperty = new GraphQLObjectType({
+const StockPropertyType = new GraphQLObjectType({
     name: 'StockProperty',
     description: 'The properties assigned to an item of stock',
     fields: () => ({
@@ -82,6 +106,12 @@ const StockProperty = new GraphQLObjectType({
                 return stockProperty.value;
             }
         },
+        stock: {
+            type: StockType,
+            resolve: (stockProperty) => {
+                return stockProperty.getStock();
+            }
+        }
     })
 });
 
@@ -89,8 +119,8 @@ const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'The root query',
     fields: () => ({
-        product: {
-            type: new GraphQLList(Product),
+        products: {
+            type: new GraphQLList(ProductType),
             args: {
                 id: { type: GraphQLInt },
                 name: { type: GraphQLString },
@@ -100,12 +130,86 @@ const RootQueryType = new GraphQLObjectType({
             resolve: (root, args) => {
                 return db.models.product.findAll({where: args})
             }
+        },
+        stock: {
+            type: new GraphQLList(StockType),
+            args: {
+                id: { type: GraphQLInt },
+                quantity: { type: GraphQLInt }
+            },
+            resolve: (root, args) => {
+                return db.models.stock.findAll({where: args})
+            }
+        },
+        stockProperties: {
+            type: new GraphQLList(StockPropertyType),
+            args: {
+                id: { type: GraphQLInt },
+                key: { type: GraphQLString},
+                value: { type: GraphQLString}
+            },
+            resolve: (root, args) => {
+                return db.models.stockProperty.findAll({where: args})
+            }
         }
     })
 });
 
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root mutation',
+    fields: () => ({
+        addProduct: {
+            type: ProductType,
+            args: {
+                name: { type: GraphQLString },
+                description: { type: new GraphQLNonNull(GraphQLString) },
+                price: { type: new GraphQLNonNull(GraphQLFloat) }
+            },
+            resolve: (_, args) => {
+                return db.models.product.create({
+                    name: args.name,
+                    description: args.description,
+                    price: args.price,
+                    discount: 0
+                })
+            }
+        },
+        addStock: {
+            type: StockType,
+            args: {
+                productId: { type: new GraphQLNonNull(GraphQLInt) },
+                quantity: { type: new GraphQLNonNull(GraphQLInt) } 
+            },
+            resolve: (_, args) => {
+                return db.models.stock.create({
+                    productId: args.productId,
+                    quantity: args.quantity
+                })
+            }
+        },
+        addStockProperty: {
+            type: StockPropertyType,
+            args: {
+                stockId: { type: new GraphQLNonNull(GraphQLInt) },
+                key: { type: new GraphQLNonNull(GraphQLString) },
+                value: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (_, args) => {
+                return db.models.stockProperty.create({
+                    stockId: args.stockId,
+                    key: args.key,
+                    value: args.value
+                })
+            }
+        }
+    })
+
+});
+
 const Schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 });
 
 module.exports = Schema;
