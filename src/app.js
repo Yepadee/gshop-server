da6b1d6ad8602fee01bd295815d489398ed9de3d
Yepadee <james@hawkinsplace.co.uk>
2020-04-16 12:21:19 +0100

    converted to apollo format

--- src/app.js
+++ src/app.js
@@ -19,41 +19,8 @@ import db from './models';
 
 import schema from './schema';
 
-const typeDefs = gql`${schema}`;
-
-const resolvers = {
-    Query: {
-        productTypes: (_, args) => {
-            return db.models.productType.findAll({where: args})
-        },
-        productPropertyNames: (_, args) => {
-            return db.models.productPropertyName.findAll({where: args})
-        },
-        productPropertyValues: (_, args) => {
-            return db.models.productPropertyValue.findAll({where: args})
-        },
-        typePropertyNames: (_, args) => {
-            return db.models.typePropertyName.findAll({where: args})
-        },
-        typePropertyValues: (_, args) => {
-            return db.models.typePropertyValue.findAll({where: args})
-        },
-        products: (_, args) => {
-            return db.models.product.findAll({where: args})
-        },
-        stock: (_, args) => {
-            return db.models.stock.findAll({where: args})
-        }
-    },
-    ProductType: {
-        products: (productType) => {
-            return productType.getProducts();
-        },
-    }
-}
-
 const PORT = 3000;
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ schema });
 
 server.listen({port:PORT}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
