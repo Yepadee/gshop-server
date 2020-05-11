730b11f9c3b7e753221942519859a877ec4eb5bd
Yepadee <james@hawkinsplace.co.uk>
2020-05-11 14:19:12 +0100

    save

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer, gql } from 'apollo-server';
-import schema from './schema';
-
-const PORT = 3000;
-const server = new ApolloServer({ schema });
-
-server.listen({port:PORT}).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
